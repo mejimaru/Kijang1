0fb0a39e66018058bce9d24fe5d179fc58b78759
mejimaru <mejimaru@gmail.com>
2013-04-19 22:53:45 +0700

    yahowu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySite::Application.config.secret_key_base = 'ba6c3bc7496761d84691463524cfed4eccdf11168ccc0b93dacded7e414697d5afe242d5391019df3f14ce611074d82d5a992952cb64ba2718e5abde03f12a80'
